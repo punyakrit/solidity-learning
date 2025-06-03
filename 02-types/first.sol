f7c806397a797e166d25294b40ad3c7cec7b39dc
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-06-04 01:08:53 +0530

    types in sol

---
+++ 02-types/first.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18; // or can use  >=0.8.18 <0.9.0
+
+contract first{
+    bool favNumber = true;
+    
+    uint numberisoo = 88; 
+    
+    uint256 numberis = 88; // 256 bits explicit how many bits are using
+    
+    int256 intNumber = -88;
+
+    string text = "80";
+
+    address muAddress = 0x5A2D9361b0B9eE704aa05b6BDeCdBF75ce239674;
+
+}
