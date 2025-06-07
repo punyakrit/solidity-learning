9f438036bf219d11d83a7f5582ed2aa89f409552
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-06-07 23:54:57 +0530

    types information

--- 02-types/first.sol
+++ 02-types/first.sol
@@ -2,17 +2,24 @@
 pragma solidity ^0.8.18; // or can use  >=0.8.18 <0.9.0
 
 contract first{
+
+    // by default values inuint is 0
+    // bool is false
+    
     bool favNumber = true;
     
-    uint numberisoo = 88; 
+    uint numberisoo = 88;   // Maximum it can got to unit256 but if not defined
+                            // by default its value is 256
     
     uint256 numberis = 88; // 256 bits explicit how many bits are using
     
     int256 intNumber = -88;
-    
 
-    string text = "80";
+
+    string text = "80"; //string are secretly bytes object specifically for text 
 
     address muAddress = 0x5A2D9361b0B9eE704aa05b6BDeCdBF75ce239674;
+
+    bytes32 fav = "abc"; // bytes with nothing by default  and 32 are different 
 
 }
