803733eed30fc11a98785b2cf74abc517d242aed
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-06-10 20:26:22 +0530

    fund me contract

--- 11-Fund_me/FundMe.sol
+++ 11-Fund_me/FundMe.sol
@@ -3,10 +3,11 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-
+import { PriceConvertor } from './PriceConvertor.sol';
 
 contract FundMe {
+
+    using PriceConvertor for uint256;
 
     uint256 public minimumUsd = 5e18;
 
@@ -14,27 +15,72 @@ contract FundMe {
 
     mapping(address => uint256) public accountSentMoney;
 
+    address public owner;
+
+    // this constructor is immediately called whenever this contract is deployed
+    constructor(){
+        owner = msg.sender;
+    }
+
+
+
+
     //payable keyword make user send money in this function
     function fund() public payable { // this function will allow users to send money
         // require states if this satisfys transction then ok otherwise reverts transaction but you will still spend gas
-        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enought eth");
+
+        // in this library used getConversionRate its first function parameter is directly taken from msg.value.getConversionRate() and second parameter would be after (20); or something else
+        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enought eth");
+        
         funders.push(msg.sender);
-        accountSentMoney[msg.sender] = accountSentMoney[msg.sender] + msg.value;
+        accountSentMoney[msg.sender] += msg.value;
     }
 
-    function getPrice() public view returns(uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 price, , ,)= priceFeed.latestRoundData();
-        return uint256(price * 1e10);
+    
+
+    function withdraw() public onlyOwner(){
+
+
+        for(uint256 i =0; i<funders.length; i++){
+            address funderUser = funders[i];
+            accountSentMoney[funderUser] = 0; 
+        }
+
+        funders = new address[](0);
+
+        //1st way is to transfer 
+        //send money from contracts to each other
+            // payable(msg.sender).transfer(address(this).balance); // if this line fails it will rever the transaction
+        //Gas Limit: Forwards 2300 gas to the recipient (enough to log an event but not enough to run complex logic).
+        // Reverts on failure: Yes (throws error if the transfer fails).
+        //  Recommended for: Simple transfers where you don’t expect the recipient to have any fallback logic.
+        // Security: Safer due to automatic revert and low gas.
+
+
+        // 2nd way send
+
+        // bool sendCheck = payable(msg.sender).send(address(this).balance);
+        // require(sendCheck,"Send failed");
+
+        //Gas Limit: Forwards 2300 gas, like transfer.
+        // Reverts on failure: No – returns false if the transfer fails.
+        // You must check the return value manually.
+        // Recommended for: Rarely used due to lack of automatic error handling.
+
+
+        // 3rd way call - is powerfull can be used to call any function in eth with having abi
+
+        (bool callCheck, ) = payable(msg.sender).call{value:address(this).balance}("");
+        require(callCheck,"Call failed");
+
+
     }
 
-    function getConversionRate(uint256 ethAmount) public view returns(uint256){
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount)/ 1e18;
-        return ethAmountInUsd;
-    }
-
-    function withdraw() public {
+// modifier is type of middleware to add a functionality to a function in its decleration
+    modifier onlyOwner(){
+        require(msg.sender == owner, "must be owner");
+        _; // this means after the above code comes the rest of the fucntion logic 
+        // if this "_;" is above then whole function code comes at top and then comes the require statement
 
     }
 }
