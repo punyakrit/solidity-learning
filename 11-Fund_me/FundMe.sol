8695296e0e3bdb9e765f214660673bc42642fa2d
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-06-11 01:26:05 +0530

    fund me optimised

--- 11-Fund_me/FundMe.sol
+++ 11-Fund_me/FundMe.sol
@@ -1,25 +1,30 @@
 // Get Funds form user, withdraw funds and allowing to give minimum usd in funds
 
+// the current gas to deploy this smart contract is too high to optimise this few basic things are while declaring values to store are constant and immutable keywords so its values cant be changed
+
+
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
 import { PriceConvertor } from './PriceConvertor.sol';
 
+error notOwner();
+
 contract FundMe {
 
     using PriceConvertor for uint256;
 
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18; // this with constant wont take upa storage spot there name are mostly in caps
 
     address[] public funders;
 
     mapping(address => uint256) public accountSentMoney;
 
-    address public owner;
+    address public immutable i_owner; // it is immutable as its value is not declared in same line and its variable also starts withi_owner
 
     // this constructor is immediately called whenever this contract is deployed
     constructor(){
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
 
@@ -30,7 +35,7 @@ contract FundMe {
         // require states if this satisfys transction then ok otherwise reverts transaction but you will still spend gas
 
         // in this library used getConversionRate its first function parameter is directly taken from msg.value.getConversionRate() and second parameter would be after (20); or something else
-        require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enought eth");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enought eth");
         
         funders.push(msg.sender);
         accountSentMoney[msg.sender] += msg.value;
@@ -78,7 +83,12 @@ contract FundMe {
 
 // modifier is type of middleware to add a functionality to a function in its decleration
     modifier onlyOwner(){
-        require(msg.sender == owner, "must be owner");
+        // -> require actually takes up a lot of gas it store a string in lot of bytes in there 
+        if(msg.sender != i_owner){
+            revert notOwner(); // this ends up saving a lot of gas
+        }
+
+        // require(msg.sender == i_owner, "must be owner");
         _; // this means after the above code comes the rest of the fucntion logic 
         // if this "_;" is above then whole function code comes at top and then comes the require statement
 
